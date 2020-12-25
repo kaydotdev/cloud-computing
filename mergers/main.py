196bd66b609b93986d7f4891fee48a55a81c5183
Anton Kozyriev <sonara4mus@gmail.com>
2020-12-25 19:26:46 +0200

    feat(merges): implement hash join algorithm

--- mergers/main.py
+++ mergers/main.py
@@ -3,17 +3,25 @@ import fire
 
 class Merger(object):
     @staticmethod
-    def hash_join(left_table, right_table):
+    def hash_join(left_table: list, left_index: int,
+                  right_table: list, right_index: int) -> list:
+        hashTable = defaultdict(list)
+
+        for s in left_table:
+            hashTable[s[left_index]].append(s)
+
+        return [(s, r) for r in right_table for s in hashTable[r[right_index]]]
+
+    @staticmethod
+    def loop_join(left_table: list, right_table: list):
         print(f'Hash joining {left_table} and {right_table}')
 
     @staticmethod
-    def loop_join(left_table, right_table):
-        print(f'Loop joining {left_table} and {right_table}')
-
-    @staticmethod
-    def merge_join(left_table, right_table):
+    def merge_join(left_table: list, right_table: list):
         print(f'Merge joining {left_table} and {right_table}')
 
 
 if __name__ == '__main__':
     fire.Fire(Merger)
+
+
