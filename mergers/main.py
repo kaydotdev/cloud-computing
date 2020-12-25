a27d44d8f6f34f5519d05505cf286eca1e2763b7
Anton Kozyriev <sonara4mus@gmail.com>
2020-12-25 20:13:23 +0200

    feat(merges): implement loop join algorithm

--- mergers/main.py
+++ mergers/main.py
@@ -13,8 +13,16 @@ class Merger(object):
         return [(s, r) for r in right_table for s in hashTable[r[right_index]]]
 
     @staticmethod
-    def loop_join(left_table: list, right_table: list):
-        print(f'Hash joining {left_table} and {right_table}')
+    def loop_join(left_table: list, left_index: int,
+                  right_table: list, right_index: int) -> list:
+        occurrences = []
+
+        for left_cortege in left_table:
+            for right_cortege in right_table:
+                if left_cortege[left_index] == right_cortege[right_index]:
+                    occurrences.append((left_cortege, right_cortege))
+
+        return occurrences
 
     @staticmethod
     def merge_join(left_table: list, right_table: list):
